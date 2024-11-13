974d2eb44bbb9221fdacf778f30bde1f695313d8
Yooshyasha <mejzikrejzi66@gmail.com>
2024-11-13 16:39:02 +0500

    Initial Commit

---
+++ src/main/kotlin/com/yooshyasha/tokenpriceapi/TokenPriceApiApplication.kt
@@ -0,0 +1,11 @@
+package com.yooshyasha.tokenpriceapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TokenPriceApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TokenPriceApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/yooshyasha/tokenpriceapi/TokenPriceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yooshyasha.tokenpriceapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TokenPriceApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
